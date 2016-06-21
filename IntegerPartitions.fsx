a7785d7056a6701399d59eecb2f00d973858311d
Adam Hickey <adam.hickey00@gmail.com>
2016-06-21 17:23:39 +0900

    Update integer partition problem.

--- IntegerPartitions.fsx
+++ IntegerPartitions.fsx
@@ -1,19 +1,20 @@
 // how many different ways can a girl jump up an n level staircase if she 
 // can jump 1, 2, or 3 steps at a time?
 
-// input: n 
-// output: list of jumps adding to n
+// 0 = []
+// 1 = [1]
+// 2 = [1; 1] [2]
+// 3 = [1; 1; 1] [1; 2] [2; 1] [3]
+// 4 = [1; 1; 1; 1] [1; 1; 2] [1; 2; 1] [1; 3] [2; 1; 1] [2; 2] [3; 1]
 
-// integer partition problem
+// integer partition2 for 1,2, and 3
+let rec partitions = function 
+  | 0 -> [[]]
+  | 1 -> [[1]]
+  | 2 -> [[1; 1]; [2]]
+  | _ as n -> 
+    [for x in partitions (n-1) -> 1::x]
+    @ [for x in partitions (n-2) -> 2::x]
+    @ [for x in partitions (n-3) -> 3::x]
 
-// n = 1
-// [1]
-
-// n = 2 
-// [1; 1] [2]
-
-// n = 3 
-// [1; 1; 1] [1; 2] [3]
-
-// n = 4 
-// [1; 1; 1; 1] [1; 1; 2] [1; 3] [2; 2] [3; 1]
+partitions 4
