bd250284ba5efa18b96c4ea427053d22ced4abe9
Adam Hickey <adam.hickey00@gmail.com>
2016-07-06 12:36:11 -0400

    Caching changes.

--- ShortestPathKnightMemoized.fsx
+++ ShortestPathKnightMemoized.fsx
@@ -24,23 +24,25 @@ let fromCache board (r,c) path (cache: (int*int) list [] []) f =
   if board |> validPosition (r,c) then    
     match cache.[r].[c] with 
     | [] -> 
-      printfn "cached1 r=%A, c=%A = %A" r c cache.[r].[c]
-      cache.[r].[c] <- path 
-      printfn "cached path=%A, cache=%A" path cache
-      f (r,c) path
-      
+      //printfn "cached1 r=%A, c=%A = %A" r c cache.[r].[c]
+      //cache.[r].[c] <- path 
+      //printfn "cached path=%A, cache=%A" path cache
+      let shortest = f (r,c) path
+      cache.[r].[c] <- shortest
+      shortest
+
     | _ as calculatedPath ->
       printfn "wtf returning cached path of %A" calculatedPath 
       // replace if current path is shorter
        
-      if List.length path < List.length calculatedPath then
-        printfn "replacing path=%A with path=%A" calculatedPath path 
-        cache.[r].[c] <- path
-        (r,c)::path
-      else
-        (r,c)::calculatedPath
+      // if List.length path < List.length calculatedPath then
+      //   printfn "replacing path=%A with path=%A" calculatedPath path 
+      //   cache.[r].[c] <- path
+      //   (r,c)::path
+      // else
+      //   (r,c)::calculatedPath
 
-      //(r,c)::calculatedPath  
+      calculatedPath  
   else 
     [] 
 
@@ -50,14 +52,20 @@ let shortestPath originalStart finish (board: int array array) =
   let cache = Array.init rows (init cols)
 
   let rec loop (r,c) path = 
-    let p1 = fromCache board (r-1,c-2) ((r,c)::path) cache loop
-    let p2 = fromCache board (r-1,c+2) ((r,c)::path) cache loop
-    let p3 = fromCache board (r+1,c-2) ((r,c)::path) cache loop
-    let p4 = fromCache board (r+1,c+2) ((r,c)::path) cache loop
-    let p5 = fromCache board (r+2,c-1) ((r,c)::path) cache loop
-    let p6 = fromCache board (r+2,c+1) ((r,c)::path) cache loop
-    let p7 = fromCache board (r-2,c-1) ((r,c)::path) cache loop
-    let p8 = fromCache board (r-2,c+1) ((r,c)::path) cache loop
+    let start = (r,c)
+    if start = originalStart then 
+      start :: path
+    else if path |> hasDuplicate then 
+      []    
+    else
+      let p1 = fromCache board (r-1,c-2) ((r,c)::path) cache loop
+      let p2 = fromCache board (r-1,c+2) ((r,c)::path) cache loop
+      let p3 = fromCache board (r+1,c-2) ((r,c)::path) cache loop
+      let p4 = fromCache board (r+1,c+2) ((r,c)::path) cache loop
+      let p5 = fromCache board (r+2,c-1) ((r,c)::path) cache loop
+      let p6 = fromCache board (r+2,c+1) ((r,c)::path) cache loop
+      let p7 = fromCache board (r-2,c-1) ((r,c)::path) cache loop
+      let p8 = fromCache board (r-2,c+1) ((r,c)::path) cache loop
 
       // let p1 = loop (r-1,c-2) (startPos::path)
       // let p2 = loop (r-1,c+2) (startPos::path)
@@ -68,17 +76,18 @@ let shortestPath originalStart finish (board: int array array) =
       // let p7 = loop (r-2,c-1) (startPos::path)
       // let p8 = loop (r-2,c+1) (startPos::path)            
 
-      // [(p1.Length, p1); (p2.Length, p2); (p3.Length, p3); (p4.Length, p4);
-      //  (p5.Length, p5); (p6.Length, p6); (p7.Length, p7); (p8.Length, p8);]
-      // |> shortestList
+      [(p1.Length, p1); (p2.Length, p2); (p3.Length, p3); (p4.Length, p4);
+       (p5.Length, p5); (p6.Length, p6); (p7.Length, p7); (p8.Length, p8);]
+      |> shortestList
 
-    cache.[r].[c]
+    //cache.[r].[c]
 
   let t = loop finish []  
   let (r,c) = originalStart
-  (r,c)::cache.[r].[c]
+  printfn "cache=%A" cache
+  cache.[r].[c]
 
-let smallBoard = [| [| 0; 1; 2 |]; [| 0; 1; 2 |] |]
+let smallBoard = [| [| 0; 1; 2 |]; [| 0; 1; 2 |]; [| 0; 1; 2 |] |]
 let mediumBoard = [| [| 0; 1; 2 |]; [| 0; 1; 2 |]; [| 0; 1; 2 |]; [| 0; 1; 2 |] |]
 let largeBoard = [| 
   [| 0; 1; 2; 3|]; // 0 
@@ -89,9 +98,27 @@ let largeBoard = [|
 
 smallBoard |> shortestPath (0,0) (1,2)
 
+mediumBoard |> shortestPath (1,1) (2,2)
+
 mediumBoard |> shortestPath (0,0) (3,2) 
 // [(0,0); (1,2); (2,0); (3,2)]
 
 //largeBoard |> shortestPath (1,1) (1,0)
 largeBoard |> shortestPath (0,0) (0,2)
-// [(0,0); (2,1); (0,2)]
+// [(0,0); (1,2); (0,2)]
+
+
+let validMoves (r,c) board = 
+  let p1 = (r-1,c-2)
+  let p2 = (r-1,c+2)
+  let p3 = (r+1,c-2)
+  let p4 = (r+1,c+2)
+  let p5 = (r+2,c-1)
+  let p6 = (r+2,c+1)
+  let p7 = (r-2,c-1)
+  let p8 = (r-2,c+1)
+
+  p1::p2::p3::p4::p5::p6::p7::[p8]
+  |> List.filter (fun x -> validPosition x board)
+
+validMoves (2,1) smallBoard  
